3319c1e8f8c6aeb2ca3c6a00bc492894894639f1
Alessio-Salerno <156314493+Alessio-Salerno@users.noreply.github.com>
2024-08-01 10:47:28 +0200

    Add files via upload

---
+++ selfwork1.php
@@ -0,0 +1,15 @@
+<?php
+
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perchè";
+$text6 = 'hai';
+$text7 = 'bevuto';
+$text8 = "tutto";
+
+echo $text1. "\n" . $text2. "\n" . $text3. "\n" . $text4. "\n" . $text5. "\n" . $text6. "\n" . $text7. "\n" . $text8. "\n"; 
+
+
+?>
